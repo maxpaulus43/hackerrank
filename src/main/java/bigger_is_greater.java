e9dfc55ed97a42d962dfed9a7e7a20653755c55e
Max Paulus <max@aerserv.com>
2018-11-04 07:48:41 -0800

    make inital commit with some problems

---
+++ src/main/java/bigger_is_greater.java
@@ -0,0 +1,70 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class bigger_is_greater {
+
+    private static String biggerIsGreater(String w) {
+        char[] chars = w.toCharArray();
+
+        // from right to left, find the first position where char[i + 1] > char[i]. Call this the 'anchor'.
+        int anchor = -1;
+        for (int i = chars.length - 2; i >= 0; i--) {
+            if (chars[i + 1] > chars[i]) {
+                anchor = i;
+                break;
+            }
+        }
+
+        // if an anchor exists, then from left to right (starting at the anchor), find the character with the
+        // smallest lexicographic delta and swap the anchor with that character. Call this character 'swap'.
+        if (anchor > -1) {
+            int d = Integer.MAX_VALUE;
+            int swap = -1;
+
+            for (int j = anchor + 1; j < chars.length; j++) {
+                if (chars[j] > chars[anchor] && chars[j] - chars[anchor] < d) {
+                    d = chars[j] - chars[anchor];
+                    swap = j;
+                }
+            }
+
+            // if a swap exists, then execute the swap and sort the characters following the anchor.
+            if (swap != -1) {
+                char tmp = chars[anchor];
+                chars[anchor] = chars[swap];
+                chars[swap] = tmp;
+
+                // after swapping, sort the characters following the anchor.
+                Arrays.sort(chars, anchor + 1, chars.length);
+                return new String(chars);
+            }
+        }
+
+        return "no answer";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int TItr = 0; TItr < T; TItr++) {
+            String w = scanner.nextLine();
+
+            String result = biggerIsGreater(w);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/counting_valleys.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class counting_valleys {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        String s = scanner.nextLine();
+        int result = countingValleys(n, s);
+        System.out.println(result);
+        scanner.close();
+    }
+
+    /**
+     * A valley has been encountered under two conditions:
+     *  The current altitude is -1 AND
+     *  The current direction is up ('U')
+     */
+    private static int countingValleys(int n, String s) {
+
+        // check for bad input here
+        if (n == 0 || s == null || s.length() == 0) { return 0; }
+
+        int valleysEncountered = 0;
+        int currentAltitude = 0;
+
+        char[] steps = s.toCharArray();
+
+        for (char step : steps) {
+            switch (step) {
+                case 'U':
+                    if (currentAltitude++ == -1) valleysEncountered++;
+                    break;
+                case 'D':
+                    currentAltitude--;
+            }
+        }
+
+        return valleysEncountered;
+    }
+}
